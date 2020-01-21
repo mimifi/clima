bc299d15ff0ac5391aae838836f28b8bcc8a9214
Mantab Tadayon <mt.tadayon@gmail.com>
2020-01-21 12:29:14 +0100

    Extract the decorationStyle in the constants file

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -40,26 +40,7 @@ class _CityScreenState extends State<CityScreen> {
                     color: Colors.black,
                     fontSize: 22.0,
                   ),
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintText: 'City Name',
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    ),
-                    icon: Icon(
-                      Icons.location_city,
-                      size: 30.0,
-                      color: Colors.white,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
-                      borderSide: BorderSide(color: Colors.white, width: 1),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                  ),
+                  decoration: kTextFieldStyle,
                 ),
               ),
               FlatButton(
